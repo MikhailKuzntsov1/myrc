926d2694556a5d2e89855a2a3e68af30f5f4979e
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2022-07-13 19:04:19 +0300

    fix gitignore * bug

--- configs/nvim/lua/user/lsp/configs.lua
+++ configs/nvim/lua/user/lsp/configs.lua
@@ -1,42 +1,46 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+    return
 end
 
-local lspconfig = require("lspconfig")
+local lsp_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lsp_status_ok then
+    return
+end
 
 -- @Warning. This is used servers. Any other servers are unused (even if installed)
 local servers = {
-	"sumneko_lua",
-	"clangd",
-	"pyright",
-	"bashls",
-	"eslint",
-	"tsserver",
-	"emmet_ls",
-	"cmake",
-	"jsonls",
-	"yamlls",
-	"html",
-	"dockerls",
-	"cssls",
-	"marksman",
+    "sumneko_lua",
+    "clangd",
+    "pyright",
+    "bashls",
+    "eslint",
+    "tsserver",
+    "emmet_ls",
+    "cmake",
+    "jsonls",
+    "yamlls",
+    "html",
+    "dockerls",
+    "cssls",
+    "marksman",
 }
 
 lsp_installer.setup({
-	ensure_installed = servers,
+    ensure_installed = servers,
 })
 
 for _, server in pairs(servers) do
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
-	local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
+    local opts = {
+        on_attach = require("user.lsp.handlers").on_attach,
+        capabilities = require("user.lsp.handlers").capabilities,
+    }
 
-	if has_custom_opts then
-		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-	end
+    local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server)
 
-	lspconfig[server].setup(opts)
+    if has_custom_opts then
+        opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+    end
+
+    lspconfig[server].setup(opts)
 end
--- configs/nvim/lua/user/lsp/null-ls.lua
+++ configs/nvim/lua/user/lsp/null-ls.lua
@@ -12,18 +12,16 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
-local code_actions = null_ls.builtins.code_actions
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
     debug = false,
     sources = {
-        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
         -- formatting.black.with({ extra_args = { "--fast" } }),
         formatting.stylua, -- lua
         diagnostics.flake8, -- python
-        code_actions.shellcheck,
         diagnostics.shellcheck,
         diagnostics.cppcheck,
 
